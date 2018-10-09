b3f029d4f344de4c14564157b5e446c96cec0755
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2018-10-09 17:26:15 +0200

    Completo código de smallsmilhandler.py

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,37 +8,48 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
         self.root_layout = {'name': 'root_layout'}
-        self.region = {}
-        self.image = {}
-        self.audio = {}
-        self.textstream = {}
+        self.region = {'name': 'region'}
+        self.image = {'name': 'image'}
+        self.audio = {'name': 'audio'}
+        self.textstream = {'name': 'textstream'}
+        self.lista = []
 
     def startElement(self, name, attrs):
         if name == 'root-layout':
             self.root_layout['width'] = attrs.get('width',"")
             self.root_layout['height'] = attrs.get('height',"")
             self.root_layout['background-color'] = attrs.get('background-color',"")
+            self.lista.append(self.root_layout)
         elif name == 'region':
             self.region['id'] = attrs.get('id',"")
             self.region['top'] = attrs.get('top',"")
             self.region['bottom'] = attrs.get('bottom',"")
             self.region['left'] = attrs.get('left',"")
             self.region['right'] = attrs.get('right',"")
+            self.lista.append(self.region)
         elif name == 'img':
             self.image['src'] = attrs.get('src',"")
             self.image['region'] = attrs.get('region',"")
             self.image['begin'] = attrs.get('begin',"")
             self.image['dur'] = attrs.get('dur',"")
+            self.lista.append(self.image)
         elif name == 'audio':
             self.audio['src'] = attrs.get('src',"")
             self.audio['begin'] = attrs.get('begin',"")
             self.audio['dur'] = attrs.get('dur',"")
+            self.lista.append(self.audio)
         elif name == 'textstream':
             self.textstream['src'] = attrs.get('src',"")
             self.textstream['region'] = attrs.get('region',"")
+            self.lista.append(self.textstream)
+
+    def get_tags(self):
+        return self.lista
 
 if __name__ == "__main__":
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
+    lista = cHandler.get_tags()
+    print(lista)
