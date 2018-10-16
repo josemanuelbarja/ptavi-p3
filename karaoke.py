49ea18f03f56bc666f09ac9f64bc24071c6c55a1
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2018-10-16 14:21:17 +0200

    Karaoke.py con objetos

--- karaoke.py
+++ karaoke.py
@@ -9,32 +9,49 @@ from xml.sax import make_parser
 
 class KaraokeLocal:
 
-    def local(self, lista):
+    def __init__(self, file):
+
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(file))
+        self.lista = cHandler.get_tags()
+
+    def __str__(self):
+
+        pal_str = ''
+        for lineas in self.lista:
+            pal_str += lineas['name']
+            for indice, valor in lineas.items():
+                if valor != '' and indice != 'name':
+                    pal_str += '\t' + indice + '=' + """ " """+valor+""" " """
+            pal_str += '\n'
+        return pal_str
+
+    def to_json(self, filesmil):
+
         smiltojson = sys.argv[1].replace('.smil','.json')
         with open(smiltojson, 'w') as fichjson:
-            json.dump(lista, fichjson, indent=4)
-        for lineas in lista:
+            json.dump(self.lista, fichjson, indent=4)
+
+    def do_local(self):
+
+        for lineas in self.lista:
             for indice, valor in lineas.items():
                 if indice == 'src':
                     if valor.startswith('http:'):
-                        loc = valor.split('/')
-                        l = str(loc[-1:])
-                        urllib.request.urlretrieve(valor, l[2:-2])
+                        loc = valor.split('/')[-1]
+                        urllib.request.urlretrieve(valor, loc)
 
 if __name__ == '__main__':
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(cHandler)
+
     try:
-        parser.parse(open(sys.argv[1]))
+        file = sys.argv[1]
     except IndexError:
-        print('Usage: python3 karaoke.py fichero.smil')
-    lista = cHandler.get_tags()
-    for lineas in lista:
-        print(lineas['name'], end = '\t')
-        for indice, valor in lineas.items():
-            if valor != '' and indice != 'name':
-                print(indice,'=', """ " """+valor+""" " """, end = '\t')
-        print(end = '\n')
-    karaoke = KaraokeLocal()
-    karaoke.local(lista)
+        sys.exit('Usage: python3 karaoke.py fichero.smil')
+    karaoke = KaraokeLocal(file)
+    #print(karaoke)
+    karaoke.to_json(file)
+    karaoke.do_local()
+    karaoke.to_json(file)
+    #print(karaoke)
